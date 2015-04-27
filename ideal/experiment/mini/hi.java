092680f8a7e5c0be50e50493e8bfc698c3307c62
Misha Dynin <dynin@google.com>
2015-04-26 18:08:35 -0700

    Added tabbed pane.

--- ideal/experiment/mini/hi.java
+++ ideal/experiment/mini/hi.java
@@ -12,25 +12,43 @@ import javax.swing.*;
 
 public class hi {
 
-  public static void showFrame() {
+  private static final String FRAME_TITLE = "ideal human interface";
+
+  private static final int FRAME_WIDTH_PX = 1000;
+  private static final int FRAME_HEIGHT_PX = 680;
+
+  private static final String[] TAB_NAMES = {
+    "Module", "Schema", "Library", "View", "Style", "Datastore"
+  };
+
+  public static void show_frame() {
     JFrame frame = new JFrame();
-    frame.setTitle("ideal human interface");
-    frame.setSize(500, 300);
+    frame.setTitle(FRAME_TITLE);
+    frame.setSize(FRAME_WIDTH_PX, FRAME_HEIGHT_PX);
     frame.setLocationRelativeTo(null);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    JLabel label = new JLabel("Hello, world!");
-    label.setHorizontalAlignment(SwingConstants.CENTER);
-    frame.getContentPane().add(label);
+    frame.getContentPane().add(make_tabbed_pane());
 
     frame.setVisible(true);
+  }
+
+  public static JTabbedPane make_tabbed_pane() {
+    JTabbedPane tab_pane = new JTabbedPane();
+    for (String tab_name : TAB_NAMES) {
+      JLabel label = new JLabel("Hello, " + tab_name + "!");
+      label.setHorizontalAlignment(SwingConstants.CENTER);
+
+      tab_pane.addTab(tab_name, label);
+    }
+    return tab_pane;
   }
 
   public static void main(String[] args) {
     SwingUtilities.invokeLater(new Runnable() {
       @Override
       public void run() {
-        showFrame();
+        show_frame();
       }
     });
   }

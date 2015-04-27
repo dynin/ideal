3941072c803373e23feb64cc7ddae9d1fc8a87a5
Misha Dynin <dynin@google.com>
2015-04-26 19:31:28 -0700

    Added status bar.

--- ideal/experiment/mini/hi.java
+++ ideal/experiment/mini/hi.java
@@ -8,7 +8,9 @@
 
 package ideal.experiment.mini;
 
+import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.*;
 
 public class hi {
 
@@ -16,6 +18,8 @@ public class hi {
 
   private static final int FRAME_WIDTH_PX = 1000;
   private static final int FRAME_HEIGHT_PX = 680;
+
+  private static final int STATUS_RIGHT_BORDER_PX = 8;
 
   private static final String[] TAB_NAMES = {
     "Module", "Schema", "Library", "View", "Style", "Datastore"
@@ -28,7 +32,10 @@ public class hi {
     frame.setLocationRelativeTo(null);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    frame.getContentPane().add(make_tabbed_pane());
+    Container content_pane = frame.getContentPane();
+    content_pane.setLayout(new BorderLayout());
+    content_pane.add(make_tabbed_pane(), BorderLayout.CENTER);
+    content_pane.add(make_status_bar(), BorderLayout.PAGE_END);
 
     frame.setVisible(true);
   }
@@ -42,6 +49,19 @@ public class hi {
       tab_pane.addTab(tab_name, label);
     }
     return tab_pane;
+  }
+
+  public static Container make_status_bar() {
+    JPanel bar = new JPanel();
+    bar.setLayout(new BorderLayout());
+
+    bar.add(new JButton("Edit"), BorderLayout.LINE_START);
+
+    JLabel status_label = new JLabel("Ok.");
+    status_label.setBorder(new EmptyBorder(0, 0, 0, STATUS_RIGHT_BORDER_PX));
+    bar.add(status_label, BorderLayout.LINE_END);
+
+    return bar;
   }
 
   public static void main(String[] args) {

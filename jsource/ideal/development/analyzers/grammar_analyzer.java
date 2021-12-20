e0b793a6e78c56f2057a535debaaf13b00dd9485
Misha Dynin <misha@dynin.com>
2021-12-20 15:40:30 -0800

    Working on handling of nonterminal symbols in grammar_analyzer. Robust error notification in grammar_analyzer.

--- jsource/ideal/development/analyzers/grammar_analyzer.java
+++ jsource/ideal/development/analyzers/grammar_analyzer.java
@@ -30,26 +30,41 @@ import ideal.development.grammars.*;
 public class grammar_analyzer extends declaration_analyzer<grammar_construct>
     implements declaration {
 
-  public static class terminal_state {
+  public static class symbol_state {
     action_name the_name;
     type the_type;
     int index;
+    origin the_origin;
 
-    public terminal_state(action_name the_name, type the_type, int index) {
+    public symbol_state(action_name the_name, type the_type, int index, origin the_origin) {
       this.the_name = the_name;
       this.the_type = the_type;
       this.index = index;
+      this.the_origin = the_origin;
+    }
+  }
+
+  public static class terminal_state extends symbol_state {
+    public terminal_state(action_name the_name, type the_type, int index, origin the_origin) {
+      super(the_name, the_type, index, the_origin);
+    }
+  }
+
+  public static class nonterminal_state extends symbol_state {
+    public nonterminal_state(action_name the_name, type the_type, int index, origin the_origin) {
+      super(the_name, the_type, index, the_origin);
     }
   }
 
   private principal_type grammar_parent;
   private int current_index;
-  private dictionary<action_name, terminal_state> symbols;
+  private dictionary<action_name, symbol_state> symbols;
+  private @Nullable error_signal result_error;
 
   public grammar_analyzer(grammar_construct the_grammar_construct) {
     super(the_grammar_construct);
     current_index = 1;
-    symbols = new hash_dictionary<action_name, terminal_state>();
+    symbols = new hash_dictionary<action_name, symbol_state>();
   }
 
   public action_name short_name() {
@@ -81,47 +96,94 @@ public class grammar_analyzer extends declaration_analyzer<grammar_construct>
       signal result = ok_signal.instance;
       readonly_list<construct> body = source.body;
       for (int i = 0; i < body.size(); ++i) {
-        if (body.get(i) instanceof terminal_construct) {
-          signal processed = process_terminal((terminal_construct) body.get(i), the_origin);
-          if (processed instanceof error_signal && result instanceof ok_signal) {
-            result = processed;
-          }
+        construct the_construct = body.get(i);
+        if (the_construct instanceof terminal_construct) {
+          process_terminal((terminal_construct) the_construct, the_origin);
+        } else if (the_construct instanceof nonterminal_construct) {
+          process_nonterminal((nonterminal_construct) the_construct, the_origin);
         }
       }
-
-      return result;
     }
 
-    return ok_signal.instance;
+    return result_error == null ? ok_signal.instance : result_error;
   }
 
   private int next_index() {
     return current_index++;
   }
 
-  private signal process_terminal(terminal_construct the_terminal_construct, origin the_origin) {
-    analyzable symbol_type = make(the_terminal_construct.the_type);
+  private void set_error(error_signal the_error_signal) {
+    if (result_error == null) {
+      result_error = the_error_signal;
+    }
+  }
+
+  private @Nullable type analyze_type(construct type_construct, origin the_origin) {
+    analyzable symbol_type = make(type_construct);
     if (has_analysis_errors(symbol_type)) {
-      return new error_signal(new base_string("Error in symbol type"), symbol_type, the_origin);
+      set_error(new error_signal(new base_string("Error in symbol type"), symbol_type, the_origin));
+      return null;
     }
 
     action action_type = action_not_error(symbol_type);
-    type the_type;
 
     if (action_type instanceof type_action) {
-      the_type = ((type_action) action_type).get_type();
+      return ((type_action) action_type).get_type();
     } else {
-      return new error_signal(messages.type_expected, symbol_type);
+      notification type_expected = new base_notification(messages.type_expected, symbol_type);
+      type_expected.report();
+      set_error(new error_signal(type_expected, true));
+      return null;
+    }
+  }
+
+  private void report_duplicate(action_name the_name, origin the_origin) {
+    notification first_symbol = new base_notification(new base_string("First symbol declaration"),
+        symbols.get(the_name).the_origin);
+    notification duplicate = new base_notification(
+        new base_string("Duplicate symbol name: " + the_name),
+        the_origin, new base_list<notification>(first_symbol));
+    duplicate.report();
+    set_error(new error_signal(duplicate, true));
+  }
+
+  private void process_terminal(terminal_construct the_terminal_construct, origin the_origin) {
+    @Nullable type the_type = analyze_type(the_terminal_construct.the_type, the_origin);
+    if (the_type == null) {
+      return;
     }
 
     for (int i = 0; i < the_terminal_construct.the_names.size(); ++i) {
       name_construct the_name_construct = the_terminal_construct.the_names.get(i);
-      terminal_state the_terminal_state = new terminal_state(the_name_construct.the_name,
-          the_type, next_index());
-      // TODO: check for duplicate symbol names.
-      symbols.put(the_terminal_state.the_name, the_terminal_state);
+      action_name the_name = the_name_construct.the_name;
+      if (symbols.contains_key(the_name)) {
+        report_duplicate(the_name, the_name_construct);
+        continue;
+      }
+      terminal_state the_terminal_state = new terminal_state(the_name, the_type, next_index(),
+          the_name_construct);
+      symbols.put(the_name, the_terminal_state);
     }
-    return ok_signal.instance;
+  }
+
+  private void process_nonterminal(nonterminal_construct the_nonterminal_construct,
+      origin the_origin) {
+    @Nullable type the_type = analyze_type(the_nonterminal_construct.the_type, the_origin);
+    if (the_type == null) {
+      return;
+    }
+
+    for (int i = 0; i < the_nonterminal_construct.the_names.size(); ++i) {
+      name_construct the_name_construct = the_nonterminal_construct.the_names.get(i);
+      action_name the_name = the_name_construct.the_name;
+      if (symbols.contains_key(the_name)) {
+        report_duplicate(the_name, the_name_construct);
+        continue;
+      }
+      nonterminal_state the_nonterminal_state = new nonterminal_state(the_name, the_type,
+          next_index(), the_name_construct);
+      symbols.put(the_name, the_nonterminal_state);
+    }
   }
 
   @Override
